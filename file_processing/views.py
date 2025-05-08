99063a18cb6b5777ddbffac59f5a067d72a29c43
Ilyas Ali Benyahia <150439305+ilyas-ali-benyahia@users.noreply.github.com>
2025-05-08 10:57:19 +0100

    Update views.py

--- file_processing/views.py
+++ file_processing/views.py
@@ -8,7 +8,7 @@ import requests
 from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_unstructured import UnstructuredLoader
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api._errors import NoTranscriptFound, TranscriptsDisabled
